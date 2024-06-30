54b25daf4db3a46ef0de34a65d5f39776002ad9a
Your Name <your_email@whatever.com>
2024-06-30 15:47:25 +0500

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Бакдаулет Турсунбаев on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
